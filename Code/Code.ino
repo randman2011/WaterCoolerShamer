12dd5e169d3c62f5879dcd1634f8d7506e7a9306
Randy Turner <randall.turner@controlsdata.com>
2016-08-30 12:00:54 -0400

    Rearrange loop for better reusability

--- Code/Code.ino
+++ Code/Code.ino
@@ -9,42 +9,54 @@ int sensor3Val = 1023;
 
 int currentWeight;
 
+int WEIGHT_THRESHOLD = 2048;
+int FAIL_THRESHOLD = 5;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  int currentWeight = getWeightValue();
+  checkWeightThreshold(currentWeight);
+
+  
+  // Compare fail count to thresholds
+  while (failCount > FAIL_THRESHOLD)
+  {
+	  
+  }
+}
+
+int getWeightValue()
+{
   // Check ADC
   sensor1Val = analogRead(pinADC1);
   sensor2Val = analogRead(pinADC2);
   sensor3Val = analogRead(pinADC3);
   
-  // Compare to thresholds
-  currentWeight = sensor1Val + sensor2Val + sensor3Val;
-  
-  if (currentWeight > emptyThreshold)
+  // Sum raw values
+  return sensor1Val + sensor2Val + sensor3Val;
+}
+
+void checkWeightThreshold(value)
+{
+  if (value > WEIGHT_THRESHOLD)
   {
-	if (failCount != 0)
-	{
-		failCount--;
-	}
-	delay(1000);
+    if (failCount != 0)
+    {
+      failCount--;
+    }
   }
   else
   {
-	if (failCount != 1023)
-	{
-		failCount++;
-	}
-	delay(1000);
-  }
-  
-  // Compare fail count to thresholds
-  if (failCount > FAIL_THRESHOLD)
-  {
-	
+    if (failCount != 1023)
+    {
+      failCount++;
+    }
   }
 }
+
